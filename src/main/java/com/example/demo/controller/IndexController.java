001ee13509b245af4a91c43017dbbf5ef471ee30
zhangxiaolong <837615651@qq.com>
2019-10-11 16:00:25 +0800

    张小龙 修改

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -4,4 +4,7 @@ public class IndexController {
     public static void main(String[] args) {
         System.out.println("aaa");
     }
+    public void creatofzxl(){
+        System.out.println("i'm zhangxiaolong");
+    }
 }
