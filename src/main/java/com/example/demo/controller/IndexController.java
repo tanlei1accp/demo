0a255994a9f61582acbbf7ac602d5c7254ca0416
yanxinyu38 <1959516256@qq.com>
2019-10-11 16:12:15 +0800

    闫新宇 提交

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -4,6 +4,7 @@ public class IndexController {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("yantijiao");
+        System.out.println("yanxinyu");
     }
 
 }
