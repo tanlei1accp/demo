11cc4233a19533b3dbd7eac0c49fdc9f35796126
xcy <xcy13722123441@163.com>
2019-10-11 15:52:27 +0800

    xcy

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -3,5 +3,6 @@ package com.example.demo.controller;
 public class IndexController {
     public static void main(String[] args) {
         System.out.println("aaa");
+        System.out.println("xcy");
     }
 }
