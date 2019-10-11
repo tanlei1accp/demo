97ca7b25e1d3aa7ae1fd95710e6e83ebb42e98fe
hzq1044690076 <1044690076@qq.com>
2019-10-11 15:58:11 +0800

    hzq提交数据请求

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -3,6 +3,6 @@ package com.example.demo.controller;
 public class IndexController {
     public static void main(String[] args) {
         System.out.println("aaa");
-        System.out.println("hzq");
+        System.out.println("hzq的提交请求");
     }
 }
